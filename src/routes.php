868de12070a20a3ed8b65973da5853461c317ed9
UA <admin@mail.localhost>
2020-01-22 18:54:56 +0200

    add flags

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php
 Route::get('/translates', 'Sashaef\TranslateProvider\Controllers\DefaultController@index')->name('translate.default.index');
+Route::get('/translate', 'Sashaef\TranslateProvider\Controllers\DefaultController@index')->name('translate.default.index');
 
 Route::group(['prefix' => Sashaef\TranslateProvider\middleware\LocaleMiddleware::getLocale()], function () {
     Route::group(['middleware' => config('translate.middleware', ['web']), 'prefix' => config('translate.url.admin', '/admin').'/translates', 'namespace' => 'Sashaef\TranslateProvider\Controllers', 'as' => 'translate.'], function () {
